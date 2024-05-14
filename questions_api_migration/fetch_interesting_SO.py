d5bf1d36988cf8e4b8d3db517627984dc5bcdb9a
wjy99-c <wangjiyuan123@yeah.net>
2024-05-14 17:10:14 +0000

    fix bugs in fectching so

--- questions_api_migration/fetch_interesting_SO.py
+++ questions_api_migration/fetch_interesting_SO.py
@@ -1,10 +1,12 @@
 import csv
+import os
 
 
 def fetch_so(api_name, app_name, file_dir):
     related_post = []
     possible_related_post = []
-    for file_count in range(600):
+    for file_count in range(577,600):
+        print("checking for the chunk: ",file_count)
         output_file_path = os.path.join(file_dir, f"split_{file_count}.xml")
         with open(output_file_path, 'r') as infile:
             for line in infile:
@@ -14,6 +16,7 @@ def fetch_so(api_name, app_name, file_dir):
                     possible_related_post.append(line)
             if len(related_post) > 100:
                 return related_post
+        print("find {0} related posts and {1} possible related post".format(str(len(related_post)), str(len(possible_related_post))))
 
     return related_post+possible_related_post
 
@@ -24,6 +27,7 @@ def get_API_name(input_csv):
         next(file)
         for line in file:
             API = line.split(',')[-1].strip()
+            API = API.split('.')[0].strip()
 
             # Remove '__call__' or '__init__' if present
             if '__call__' in API:
@@ -37,12 +41,13 @@ def get_API_name(input_csv):
 
 
 app_name = 'langchain'
-output_csv_path = "apis_python_posts_{0}.csv".format(app_name)
-API_names = get_API_name("API_results/deprecated_apis_general_{0}.csv".format(app_name))
+API_names = get_API_name("/mnt/ssd/jiyuan/toy_llm_rewrites/questions_api_migration/API_results/deprecated_apis_general_{0}.csv".format(app_name))
 print(API_names)
-API_related_posts = fetch_so(api_name="profile.start", app_name=app_name, file_dir="SO_chunk_by_line")
-with open(output_csv_path, mode="w", newline="", encoding="utf-8") as csvfile:
-    for post in API_related_posts:
-        csvfile.write(post)
+for API_name in API_names:
+    output_csv_path = "langchain_post/apis_python_posts_{0}_{1}.csv".format(app_name,API_name)
+    API_related_posts = fetch_so(api_name="profile.start", app_name=app_name, file_dir="/mnt/ssd/dataset/SO_chunk_by_line")
+    with open(output_csv_path, mode="w", newline="", encoding="utf-8") as csvfile:
+        for post in API_related_posts:
+            csvfile.write(post)
 
-print(f"Results have been saved to {output_csv_path}")
+    print(f"Results have been saved to {output_csv_path}")
