a6ce8177fff57a12c73ff211a4874fafdb05493e
Kang Hong Jin <kanghongjin@gmail.com>
2024-05-23 13:46:09 -0700

    support gpt-4, and track error messages

--- questions_py2_to_py3/check_python_versions.py
+++ questions_py2_to_py3/check_python_versions.py
@@ -5,14 +5,13 @@ import os
 import re
 
 def check_python_version():
-    # python_versions = ['python:2', 'python:3']
     python_versions = ['py2_env', 'py3_env'] # python images whose Dockerfiles are found in the `dockers` directory`
     results = {}
+    errors = {}
 
     pwd = os.getcwd()
 
     for version in python_versions:
-        # Create and run the docker container
         try:
             print(f"Testing on {version}...")
             print("docker", "run", "--rm", "-v", pwd + "/tmpdir:/workspace", version)
@@ -27,7 +26,18 @@ def check_python_version():
             results[version] = 0
             print(f"Failed: The script does not work on {version}.\nError:\n{e.output.decode('utf-8')}")
 
-    return results
+            # read the error message
+            error_message = e.output.decode('utf-8')
+            # print(">>>>>>>>>>>>>>>>>>>", error_message)
+            # find the line with "Error:"
+            error_line = re.search(r'(\w+Error: .*)', error_message)
+            print(">>>>>>>>>>>>>>>>>>>", error_line)
+            if error_line:
+                errors[version] = error_line.group(1)
+            else:
+                errors[version] = ''
+
+    return results, errors
 
 def extract_code_in_code_tags(text):
     # extract code wrapped in <code></code> tags
@@ -60,20 +70,30 @@ if __name__ == "__main__":
 
     # count outcomes
     outcomes = {}
-    with open(sys.argv[1], mode='r', newline='', encoding='utf-8') as infile:
-        reader = csv.DictReader(infile)
 
-        # extract code-looking stuff
+    error_types = {}
+    with open(sys.argv[1], mode='r', newline='', encoding='utf-8') as infile,\
+         open(sys.argv[1].split('.csv')[0] +'_output.csv', 'w') as outfile:
+        reader = csv.DictReader(infile)
+        writer = csv.DictWriter(outfile, fieldnames=reader.fieldnames + 
+                                ['rag_response_py2_outcome', 'rag_response_py3_outcome', 
+                                 'no_rag_response_py2_outcome', 'no_rag_response_py3_outcome',
+                                 'rag_response_py2_err', 'rag_response_py3_err', 
+                                 'no_rag_response_py2_err', 'no_rag_response_py3_err'])
+
+        writer.writeheader()
+
         total_rows = 0
         for row in reader:
             query = row['query']
             total_rows += 1
 
+            output_row = row.copy()
             for type_of_response in ['rag_response', 'no_rag_response']:
                 response = row[type_of_response] 
 
                 print('=========Row========')
-                print(response)
+                # print(response)
 
                 code_snippets = extract_code_in_code_tags(response) + extract_code_in_markdown_code_blocks(response)
 
@@ -81,17 +101,17 @@ if __name__ == "__main__":
                     code_snippets.append(response)
 
                 print('extracted code snippets of size=' , len(code_snippets))
-                print(code_snippets)
+                # print(code_snippets)
                 
                 for snippet in code_snippets:
                     with open(file_path, 'w') as f:
                         f.write(snippet)
 
-                    with open(file_path, 'r') as f:
-                        print(f.read())
+                    # with open(file_path, 'r') as f:
+                    #     print(f.read())
 
                     # check the python versions of the file in script.py
-                    version_outcomes = check_python_version()
+                    version_outcomes, version_errors = check_python_version()
                     print('===================')
                     print(version_outcomes)
 
@@ -101,8 +121,26 @@ if __name__ == "__main__":
                         if version not in outcomes[type_of_response]:
                             outcomes[type_of_response][version] = 0
                         outcomes[type_of_response][version] += outcome
-                        
+
+                    for version, error in version_errors.items():
+                        if type_of_response not in error_types:
+                            error_types[type_of_response] = {}
+                        if version not in error_types[type_of_response]:
+                            error_types[type_of_response][version] = []
+                        error_types[type_of_response][version].append(error)
+                    break
+
+                if len(version_outcomes) > 0:
+                    output_row[type_of_response + '_py2_outcome'] = version_outcomes['py2_env']
+                    output_row[type_of_response + '_py3_outcome'] = version_outcomes['py3_env']
+                if len(version_errors) > 0:
+                    if 'py2_env' in version_errors:
+                        output_row[type_of_response + '_py2_err'] = version_errors['py2_env']
+                    if 'py3_env' in version_errors:
+                        output_row[type_of_response + '_py3_err'] = version_errors['py3_env']
+                print(output_row)
+                writer.writerow(output_row)
+
         print("=================================")
         print("Final Results")
         print(outcomes)
-
--- questions_py2_to_py3/query.py
+++ questions_py2_to_py3/query.py
@@ -1,21 +1,27 @@
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_core.prompts import ChatPromptTemplate
+
 from langchain.chains import RetrievalQA
-from langchain_community.llms import OpenAI
 from langchain.prompts import PromptTemplate
 
 import os
 import sys
 
-# os.environ['OPENAI_API_KEY'] = 'key'
-
 embedding_model = OpenAIEmbeddings()
 
 db = sys.argv[1]
 
+model = sys.argv[2]
+
 vector_store = FAISS.load_local(db, embedding_model, allow_dangerous_deserialization=True)
 
-llm = OpenAI(model_name="gpt-3.5-turbo-instruct") 
+if model == 'gpt-3.5-turbo-instruct':
+    from langchain_community.llms import OpenAI
+    llm = OpenAI(model_name="gpt-3.5-turbo-instruct") 
+elif model == 'gpt-4-turbo':
+    from langchain_community.chat_models import ChatOpenAI
+    llm = ChatOpenAI(model_name="gpt-4-turbo")
 
 template = """
 You are to answer questions in the style of a user on StackOverflow answering questions. You MUST provide a code snippet in your answer.
@@ -36,21 +42,64 @@ def ask_question(query):
     retrieved_docs = vector_store.similarity_search(query,k=3)
 
     formatted_context = "\n".join([doc.page_content[:1500] for doc in retrieved_docs])
-    final_prompt = template.format(context=formatted_context, question=query)
+    if model == 'gpt-3.5-turbo-instruct':
+            
+        final_prompt = template.format(context=formatted_context, question=query)
 
-    print('===================')
-    print("\nPrompt to LLM")
-    print(final_prompt)
-    print("Response with RAG:")
-    rag_response = llm(final_prompt)
-    print(rag_response)
-    print('===================')
+        print('===================')
+        print("\nPrompt to LLM")
+        print(final_prompt)
+        print("Response with RAG:")
+        rag_response = llm(final_prompt)
+        print(rag_response)
+        print('===================')
 
-    # if we didn't use RAG,
-    no_rag_response = llm(query)
-    print('response without RAG:')
-    print(no_rag_response)
-    print('===================')
+        # if we didn't use RAG,
+        no_rag_response = llm(query)
+        print('response without RAG:')
+        print(no_rag_response)
+        print('===================')
+    else:
+        prompt = """
+        You are to answer questions in the style of a user on StackOverflow answering questions. You MUST provide a code snippet in your answer.
+        Ensure that all imports are included in the code.
+        You are given the following example transformations (provided in diff format) for rewriting code snippets in your response. Added code is prefixed by +, and removed code prefixed by -.:
+        {context}
+
+        Answer the following question, but transform your answer based on the above diffs/transformations (remove code prefixed with -, and add the code prefixed with +, but do NOT format any part of the answer as a diff)."""
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", prompt),
+                ("human", "{question}")
+
+            ]
+        )
+
+        chain = prompt | llm
+        rag_response = chain.invoke({"context": formatted_context, "question": query}).content
+        
+
+        no_rag_prompt = """You are to answer questions in the style of a user on StackOverflow answering questions. You MUST provide a code snippet in your answer.
+        Ensure that all imports are included in the code. Answer the following question, using only Python3 code
+        """
+        no_rag_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", no_rag_prompt),
+                ("human", "{question}")
+            ]
+        )
+
+        no_rag_chain = no_rag_prompt | llm
+        no_rag_response = no_rag_chain.invoke({"question": query}).content
+
+        print('===================')
+        print("Response with RAG:")
+        print(rag_response)
+        print('===================')
+        print('response without RAG:')
+        print(no_rag_response)
+        print('===================')
+
 
     return (rag_response, no_rag_response)
 
@@ -65,16 +114,22 @@ with open('query.csv', 'r') as i_f,\
     writer_no_rag = csv.writer(o_f_no_rag)
     writer_full = csv.writer(full)
 
-    next(reader)
+    headers = next(reader)
 
     # write headers
-    writer_rag.writerow(['rag_response'])
-    writer_no_rag.writerow(['no_rag_response'])
-    writer_full.writerow(['query', 'rag_response', 'no_rag_response', 'original_answer'])
+    # reuse the same header from the query file
+    # plus one column for the generated response
+    
+    writer_rag.writerow(headers + ['rag_response'])
+    writer_no_rag.writerow(headers + ['no_rag_response'])
+    writer_full.writerow(headers+ ['rag_response', 'no_rag_response', 'original_answer'])
     for row in reader:
         rag, no_rag = ask_question(row[0])
+        rag_row = row + [rag]
+        writer_rag.writerow(rag_row)
 
-        writer_rag.writerow(rag)
-        writer_no_rag.writerow(no_rag)
-        writer_full.writerow([row[0], rag, no_rag, row[1]])
+        no_rag_row = row + [no_rag]
+        writer_no_rag.writerow(no_rag_row)
 
+        full_row = row + [rag, no_rag]
+        writer_full.writerow(full_row)
